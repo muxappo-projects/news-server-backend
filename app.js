760856418e3827eb8d99387bb88d4e14069c8f30
Max Field <maxfield98@outlook.com>
2023-10-04 15:59:10 +0100

    Added error controllers file

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 const {
+  create404,
+  handlePSQLErrors,
+  noEndpoint,
+} = require("./controllers/error.controllers.js");
+
+const {
   getTopics,
   getAllEndpoints,
   getArticleByID,
   getAllArticles,
   getCommentsByArticle,
   postComment,
-} = require("./controllers/controllers.js");
+} = require("./controllers/server.controllers.js");
 
 app.use(express.json());
 
@@ -21,40 +27,11 @@ app.get("/api/articles/:article_id/comments", getCommentsByArticle);
 // POST/PATCH requests
 app.post("/api/articles/:article_id/comments", postComment);
 
-app.use((req, res, next) => {
-  const error = new Error("Endpoint does not exist");
-  error.status = 404;
-  next(error);
-});
-
 // error handling
+app.use(create404);
 
-app.use((err, req, res, next) => {
-  let errStatus;
-  switch (err.code) {
-    case "22P02":
-      errStatus = 400;
-      err.message = "Invalid input format";
-      break;
+app.use(handlePSQLErrors);
 
-    case "23502":
-      errStatus = 400;
-      err.message = "Required field(s) missing";
-      break;
-
-    case "23503":
-      errStatus = 404;
-      err.message = "Not found";
-      break;
-
-    default:
-      return next(err);
-  }
-  res.status(errStatus).send({ msg: err.message });
-});
-
-app.use((err, req, res, next) => {
-  res.status(err.status).send({ msg: err.message });
-});
+app.use(noEndpoint);
 
 module.exports = app;
