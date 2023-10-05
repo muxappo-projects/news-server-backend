58d82fe8780deec3316b45512f6641283a22f2c7
Max Field <maxfield98@outlook.com>
2023-10-05 16:59:15 +0100

    changing branch

--- models/models.js
+++ models/models.js
@@ -16,9 +16,18 @@ exports.fetchTopics = () => {
 };
 
 exports.fetchArticleByID = (id) => {
+  console.log(id);
   const getQuery = `
-    SELECT * FROM articles
-    WHERE article_id = $1
+  SELECT
+    articles.author, articles.article_id,
+    articles.title, articles.topic,
+    articles.created_at, articles.votes,
+    articles.article_img_url,
+  COUNT (comments.comment_id) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON articles.article_id = comments.article_id
+  WHERE articles.article_id = $1
+  GROUP BY articles.article_id;
     `;
   return db.query(getQuery, [id]).then(({ rows }) => {
     if (rows.length === 0) {
